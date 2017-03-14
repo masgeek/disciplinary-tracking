a35a206d1dee7db937b240baf5aceaae35b2ba45
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-14 12:43:52 +0300

    updated ftp folder
    Signed-off-by: Sammy Barasa <sammy.barasa@pristone.net>

--- config/web.php
+++ config/web.php
@@ -52,12 +52,19 @@ $config = [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
-                '/' => 'site',
+                //default rules
+
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+                //custom rules
+                '/' => 'site/index',
                 'discipline' => 'disciplinarytype/index',
                 'casetypes' => 'casetype/index',
                 'add-case' => 'studentincidence/create'
+
             ],
-        ],
+        ]
 
 
     ],
