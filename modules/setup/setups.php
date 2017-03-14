6329272aabe043abbbbc590860ae58be49999e00
Musyoka <mnmutisya@gmail.com>
2017-03-14 12:04:53 +0300

    setups

---
+++ modules/setup/setups.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace app\modules\setup;
+
+/**
+ * setups module definition class
+ */
+class setups extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'app\modules\setup\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
