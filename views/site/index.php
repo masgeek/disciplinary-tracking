cf86b7f0e5381262671f57a131b044246aae63c9
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-15 11:18:27 +0300

    migration logic update
    Signed-off-by: Sammy Barasa <sammy.barasa@pristone.net>

--- views/site/index.php
+++ views/site/index.php
@@ -7,9 +7,9 @@ $this->title = 'My Yii Application';
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Welcome to the</h1>
+        <h1>University of Nairobi</h1>
 
-        <p class="lead">University of Nairobi Disciplinary Tracking System</p>
+        <p class="lead">Welcome to the Disciplinary Tracking System</p>
 
         <p><a class="btn btn-lg btn-success" href="#">Select an item below to get started</a></p>
     </div>
