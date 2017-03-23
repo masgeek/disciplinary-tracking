d269e9c44b4fefa66e5b55f0e490bd85e019e760
Sammy Barasa <sammy.barasa@pristone.net>
2017-03-23 16:02:11 +0300

    refactoring of functions
    Signed-off-by: Sammy Barasa <sammy.barasa@pristone.net>

--- modules/reporting/views/progress/first-office.php
+++ modules/reporting/views/progress/first-office.php
@@ -5,12 +5,20 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $tracking \app\modules\reporting\models\TRACKING_MODEL */
 /* @var $process_actor \app\modules\reporting\models\PROCESS_ACTOR_MODEL */
+/* @var $nextProcess \app\modules\setup\models\PROCESS_MODEL */
 /* @var $incidence \app\models\STUDENT_INCIDENCE */
 /* @var $form yii\widgets\ActiveForm */
 
 $case_name_arr = \app\modules\reporting\models\CASE_MODEL_VIEW::GetCaseNameArray($incidence->INCIDENCE_ID);
 
-$p = \app\modules\setup\models\PROCESS_MODEL::GetNextTrackingProcess($incidence->CASE_TYPE_ID, false);
+
+$contains_one_process = \app\modules\reporting\models\TRACKING_MODEL::GetTrackedProcesses($incidence->INCIDENCE_ID);
+
+$nextProcess = \app\modules\setup\models\PROCESS_MODEL::GetNextTrackingProcess($incidence->CASE_TYPE_ID, false, $contains_one_process);
+
+$process_actors = \app\modules\reporting\models\PROCESS_ACTOR_MODEL::GetProcessActors($nextProcess->PROCESS_ID);
+
+var_dump($nextProcess);
 ?>
 <h1>progress/first-office</h1>
 
@@ -18,9 +26,10 @@ $p = \app\modules\setup\models\PROCESS_MODEL::GetNextTrackingProcess($incidence-
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($incidence, 'CASE_TYPE_ID')->dropDownList($case_name_arr) ?>
-    <?= $form->field($tracking, 'INCIDENCE_ID')->textInput(['value' => $incidence->INCIDENCE_ID]) ?>
-    <?= $form->field($tracking, 'PROCESS_ID')->textInput(['value' => $p->PROCESS_ID]) ?>
+    <?= $form->field($incidence, 'CASE_TYPE_ID')->dropDownList($case_name_arr)->label(false) ?>
+    <?= $form->field($tracking, 'INCIDENCE_ID')->hiddenInput(['value' => $incidence->INCIDENCE_ID])->label(false) ?>
+    <?= $form->field($tracking, 'PROCESS_ID')->hiddenInput(['value' => $nextProcess->PROCESS_ID])->label(false) ?>
+    <?= $form->field($tracking, 'COMMENTS')->textarea(['rows' => 6]) ?>
     <?= $form->field($process_actor, 'PROCESS_ACTOR_ID')->textInput(['maxlength' => true]) ?>
 
     <div class="form-group">
