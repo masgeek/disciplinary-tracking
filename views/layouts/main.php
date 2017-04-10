3fe51dbaaa9a29e1fece6b16de1f94249a59da16
Sammy Barasa <sammy.barasa@pristone.net>
2017-04-10 14:18:15 +0300

    Added missing semicolon
    Signed-off-by: Sammy Barasa <sammy.barasa@pristone.net>

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -40,8 +40,8 @@ use yii\widgets\Breadcrumbs;
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            //['label' => 'About', 'url' => ['/site/about']],
+            //['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
             ['label' => 'Login', 'url' => ['/site/login']]
             ) : (
