6e5a737c00afc5af0a40cc2e0bccb8fc52d24759
Sammy Barasa <sammy.barasa@pristone.net>
2017-04-12 13:03:41 +0300

    Updates
    Signed-off-by: Sammy Barasa <sammy.barasa@pristone.net>

--- config/web.php
+++ config/web.php
@@ -116,7 +116,7 @@ if (YII_ENV_DEV) {
     $config['bootstrap'][] = 'gii';
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        //'allowedIPs' => ['127.0.0.1', '::1', '41.89.65.170'],
+        'allowedIPs' => ['127.0.0.1', '::1', '41.89.65.170'],
         'generators' => [
             'model' => [
                 'class' => 'yii\gii\generators\model\Generator',
