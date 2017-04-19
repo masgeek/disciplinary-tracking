e18a018e5ecadff5ad50aded929aafdacd7789ab
Sammy Barasa <sammy.barasa@pristone.net>
2017-04-19 15:17:28 +0300

    removed unnecessary redirection in first tracking instance
    Signed-off-by: Sammy Barasa <sammy.barasa@pristone.net>

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -39,7 +39,7 @@ use yii\widgets\Breadcrumbs;
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Home', 'url' => ['/']],
             //['label' => 'About', 'url' => ['/site/about']],
             //['label' => 'Contact', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
